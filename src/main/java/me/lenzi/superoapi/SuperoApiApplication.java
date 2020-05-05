3740c7116fa8e55517175f47da8437d49405f8ed
Fabio Lenzi <fabio.lenzi@grey.com>
2020-05-05 19:20:34 -0300

    Adding initial files

---
+++ src/main/java/me/lenzi/superoapi/SuperoApiApplication.java
@@ -0,0 +1,13 @@
+package me.lenzi.superoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/lenzi/superoapi/SuperoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lenzi.superoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
